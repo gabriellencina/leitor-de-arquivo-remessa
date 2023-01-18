9fc39f671a40318ad0e49fd618914e74b28b021e
Gabriel Lencina <54336473+gabriellencina@users.noreply.github.com>
2023-01-18 09:39:45 -0300

    Delete connection.php

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-define ('HOST','mysql.intranetfuturo.com.br');
-define ('USER','intranetfuturo01');
-define ('PASS','Futuro2022');
-define ('BASE','intranetfuturo01');
-
-$connection = new mysqli(HOST, USER, PASS, BASE); 
